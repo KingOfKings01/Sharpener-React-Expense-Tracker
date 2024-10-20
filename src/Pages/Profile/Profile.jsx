bdd3fbff15bb4889ce39a2e0566f9369440b003b
KingOfKings01 <aasifkhan9605@gmail.com>
2024-10-20 20:13:19 +0530

    Fetch previouse information of user profile and set it on form.

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -1,63 +1,80 @@
 import { useEffect, useState } from 'react';
 import { getUserProfile, updateUserProfile } from '../../Firebase/authFun';
 import classes from "./profile.module.css"
+import { FaGithub } from "react-icons/fa";
+import { IoMdLink } from 'react-icons/io';
 
 export default function Profile() {
-  const [fName, setFName] = useState('');
-  const [imgUrl, setImgUrl] = useState('');
+    const [fName, setFName] = useState('');
+    const [imgUrl, setImgUrl] = useState('');
 
-  useEffect(()=>{
-    fetchUserProfile()
-})
+    const [isLoading, setIsLoading] = useState(false);
 
-async function fetchUserProfile() {
-      const data =  await getUserProfile()
+    useEffect(() => {
+        fetchUserProfile()
+    })
 
-      if (data) {
-        setFName(data.displayName);
-        setImgUrl(data.photoURL);
-      } else {
-        console.error('No user profile data found');
-      }
+    async function fetchUserProfile() {
+        setIsLoading(true);
+        try {
+            const data = await getUserProfile()
+            if (data) {
+                setFName(data.displayName);
+                setImgUrl(data.photoURL);
+            } else {
+                throw new Error('No user profile data found');
+            }
+        } catch (error) {
+            console.error(error.message)
+        }
+        setIsLoading(false);
     }
 
-  
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    console.log({ fName, imgUrl });
 
-    await updateUserProfile(fName, imgUrl);
+    const handleSubmit = async (event) => {
+        event.preventDefault();
 
-    setFName('');
-    setImgUrl('');
-  };
+        console.log({ fName, imgUrl });
 
-  return (
-    <section className={classes.formGroup}>
-      <h3>Contact Details</h3>
-      <form onSubmit={handleSubmit}>
-        <div className={classes.formControl}>
-          <label htmlFor="fName">Full Name</label>
-          <input
-            type="text"
-            id="fName"
-            name="fName"
-            value={fName}
-            onChange={(e) => setFName(e.target.value)}
-          />
-        </div>
-        <div className={classes.formControl}>
-          <label htmlFor="imgUrl">Profile Photo URL</label>
-          <input
-            type="text"
-            id="imgUrl"
-            name="imgUrl"
-            value={imgUrl}
-            onChange={(e) => setImgUrl(e.target.value)}
-          />
-        </div>
-        <button type="submit">Update</button>
-      </form>
-    </section>
-  );
+        await updateUserProfile(fName, imgUrl);
+
+        setFName('');
+        setImgUrl('');
+    };
+
+    return (
+        <section className={classes.container}>
+            <h3>Contact Details</h3>
+            {isLoading ? <p>Please wait...</p> :
+
+                <form className={classes.formGroup} onSubmit={handleSubmit}>
+                    <div className={classes.formControl}>
+
+                        <FaGithub />
+                        <label htmlFor="fName"> Full Name: </label>
+                        <input
+                            type="text"
+                            id="fName"
+                            name="fName"
+                            value={fName}
+                            onChange={(e) => setFName(e.target.value)}
+                        />
+                        <IoMdLink />
+                        <label htmlFor="imgUrl"> Profile Photo URL: </label>
+                        <input
+                            type="text"
+                            id="imgUrl"
+                            name="imgUrl"
+                            value={imgUrl}
+                            onChange={(e) => setImgUrl(e.target.value)}
+                        />
+                    </div>
+                    <button type="submit">Update</button>
+                </form>
+            }
+
+            <hr />
+
+        </section>
+    );
 }
